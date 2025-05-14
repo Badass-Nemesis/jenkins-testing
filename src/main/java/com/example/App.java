f20e618829e88909be4f6364ae4868cdf0f7c211
BadassNemesis <harshitanant29@gmail.com>
2025-05-14 08:44:32 +0000

    updated xml and java code to use normal webserver instead of spring-boot

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,19 +1,22 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-@SpringBootApplication
-@RestController
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello from Maven & Spring Boot!";
+    public static void main(String[] args) throws IOException {
+        // Use minimal thread pool (1 thread for t2.micro)
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 1); // <- Backlog set to 1
+        server.createContext("/", exchange -> {
+            String response = "Hello from lightweight Java HTTP Server!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+        server.start();
+        System.out.println("Server running on http://localhost:9090 (Press Ctrl+C to stop)");
     }
 }
